f8ca8f42b61ac2f940d484cb8a442b553b9d7a71
Robert Fox <robertpaulfox@gmail.com>
2020-02-28 11:38:56 +0200

    Updates

---
+++ precache-manifest.83c938e7c967cec727edb425c5ddc37a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05141951e21eb0aca6ede93178e66cbd",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "19139834cb625ccc10ac",
+    "url": "/monsters-rolodex/static/css/main.13c52d0c.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19139834cb625ccc10ac",
+    "url": "/monsters-rolodex/static/js/main.b181f006.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
